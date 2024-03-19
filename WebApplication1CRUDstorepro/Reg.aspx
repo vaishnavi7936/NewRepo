9e74bc4310c6451704fda0307c3d2415ff067253
Girish Giradkar <girishgiradkar824@gmail.com>
2024-03-20 00:43:42 +0530

    Add project files.

---
+++ WebApplication1CRUDstorepro/Reg.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reg.aspx.cs" Inherits="WebApplication1.Reg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <style>
+        #bg {
+            background-color:cadetblue;
+        }
+    </style>
+</head>
+<body id="bg">
+    <form id="form1" runat="server">
+        <div>
+            Roll&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtroll" runat="server" BorderWidth="3px"></asp:TextBox>
+&nbsp;&nbsp;
+           <%-- <asp:Button ID="btnfind" runat="server" Text="find" BackColor="Red" ForeColor="White" Width="101px" OnClick="btnfind_Click" />--%>
+            <br />
+            <br />
+            Name&nbsp;&nbsp;
+            <asp:TextBox ID="txtname" runat="server" BorderWidth="3px"></asp:TextBox>
+            <br />
+            <br />
+            City&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtcity" runat="server" BorderWidth="3px"></asp:TextBox>
+            <br />
+            <br />
+            Age&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtage" runat="server" BorderWidth="3px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnsubmit" runat="server" Text="submit" BackColor="Red" ForeColor="White" Width="104px" OnClick="btnsubmit_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnupdate" runat="server" Text="update" BackColor="Red" ForeColor="White" Width="103px" OnClick="btnupdate_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;
+            <asp:Button ID="btndelete" runat="server" Text="delete" BackColor="Red" ForeColor="White" Width="88px" OnClick="btndelete_Click" />
+           
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1CRUDstorepro/Reg.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using BusinessLogic;
+using BusinessObject;
+
+namespace WebApplication1
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        UserBL b3 = new UserBL();
+        UserBO objbo = new UserBO();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            { }
+        }
+
+        
+            
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            int Returnvalue = 0;
+            objbo.Roll = int.Parse(txtroll.Text);
+            objbo.Name = txtname.Text;
+            objbo.City = txtcity.Text;
+            objbo.Age = int.Parse(txtage.Text);
+
+            Returnvalue = b3.savedata(objbo);
+
+        }
+    
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            int Returnvalue = 0;
+            objbo.Roll = int.Parse(txtroll.Text);
+            objbo.Name = txtname.Text;
+            objbo.City = txtcity.Text;
+            objbo.Age = int.Parse(txtage.Text);
+
+            Returnvalue = b3.Updatasave(objbo);
+
+        }
+
+        protected void btndelete_Click(object sender, EventArgs e)
+        {
+
+            int Returnvalue = 0;
+            objbo.Roll = int.Parse(txtroll.Text);
+            Returnvalue = b3.Deletesave(objbo);
+
+        }
+
+        //protected void btnfind_Click(object sender, EventArgs e)
+        //{
+        //    int r = 0;
+
+        //    bo.Roll = int.Parse(txtroll.Text);
+
+        //    r = bl.find(bo);
+
+        //    if (r > 0)
+        //    {
+        //        txtname.Text = bo.Name.ToString();
+        //        txtcity.Text = bo.City.ToString();
+        //        txtage.Text = bo.Age.ToString();
+
+        //    }
+        //    else
+        //    {
+        //        Response.Write("<script>alert('Not Found');</script>");
+        //    }
+
+        //}
+    }
+
+}
+    
